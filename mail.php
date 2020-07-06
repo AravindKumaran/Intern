f0dd806fbdead2337ead3d9deee4fae56ac1394b
Asfiya <asfiya@vinteksolution.com>
2020-07-06 21:26:12 +0530

    #Intern - Changes

--- mail.php
+++ mail.php
@@ -11,7 +11,7 @@ $subject='Vintek Solution Intern Response';
 $message = "<strong>Name: </strong>".$Name."<br><strong>E-mail ID: </strong>".$email."<br><strong>Mobile number: </strong>".$mobile."<br><strong>Message: </strong>".$mes;
 
 echo "<script>alert('Thank you for the interest!');";
-echo  "location.replace(\"http://www.learn.vinteksolution.com/\");</script>";
+echo  "location.replace(\"http://learn.vinteksolution.com/\");</script>";
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 echo mail($to, $subject, $message, $headers);
